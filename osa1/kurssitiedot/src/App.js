511d250ce0ab793db723658b685cbaee5553d3d3
Jyri Ojala <jyri.ojala@web-veistamo.fi>
2021-11-14 22:32:40 +0200

    Test examples before task 1.6.

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -1,4 +1,54 @@
 
+import React, { useState } from 'react'
+
+
+const Display = ({counter}) => <span> {counter} </span>
+const Button = ({handleClick, buttonText}) => (
+    <button onClick={handleClick}>{buttonText}</button>
+)
+
+const History = ( {clicks} ) => {
+
+  if ( clicks.length === 0) {
+    return <p>Ei tallennettuja arvoja vielä. Painele namiskoita siis.</p> 
+  } else {
+    return <p>{clicks.join(',')}</p>
+  }
+}
+
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  
+  const increaseLeft = () => {
+    setAll(allClicks.concat('V'))
+    setLeft(left + 1)
+  }
+  const increaseRight = () => {
+    setAll(allClicks.concat('O'))
+    setRight(right + 1)
+  }
+
+  const resetAll = () => {
+    setLeft(0)
+    setRight(0)
+    setAll([])
+  }
+
+  return (
+    <>
+      <Display counter={left}/><Button handleClick={increaseLeft} buttonText="vasen"/>
+      <Button handleClick={increaseRight} buttonText="oikea"/><Display counter={right}/>
+      <History clicks={allClicks}/>
+      <Button handleClick={resetAll} buttonText="resetoi kaikki"/>
+    </>
+  )
+}
+
+export default App
+
+/*
 const Header = (props) => {
   return (
     <>
@@ -61,6 +111,5 @@ const App = () => {
     </div>
   )
 }
+*/
 
-
-export default App
