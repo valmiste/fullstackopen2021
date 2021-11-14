c010927adbe16bb892ece724b2f294e028fcd3d7
Jyri Ojala <jyri.ojala@web-veistamo.fi>
2021-11-14 23:03:33 +0200

    Add unicafe task answer for 1.11

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -1,47 +1,73 @@
 
 import React, { useState } from 'react'
 
-
-const Display = ({counter}) => <span> {counter} </span>
+const StatisticLine = ({value, text}) => (
+  <> 
+    <tr>
+      <td> {text} </td>
+      <td>&nbsp;</td>
+      <td>{value} </td>
+    </tr>
+  </>
+)
 const Button = ({handleClick, buttonText}) => (
     <button onClick={handleClick}>{buttonText}</button>
 )
 
-const History = ( {clicks} ) => {
-
-  if ( clicks.length === 0) {
-    return <p>Ei tallennettuja arvoja vielä. Painele namiskoita siis.</p> 
+const Statistics = ({good, neutral, bad, all, average, positive}) => {
+  if (all === 0 ) {
+    return <p>No feedback given</p>
   } else {
-    return <p>{clicks.join(',')}</p>
+    return (
+      <>
+      <tbody>
+      <StatisticLine text="good" value={good}/>
+      <StatisticLine text="neutral" value={neutral}/>
+      <StatisticLine text="bad" value={bad}/>
+      <StatisticLine text="all" value={all}/>
+      <StatisticLine text="average" value={ average }/>
+      <StatisticLine text="positive" value={ positive }/>
+      </tbody>
+      </>
+    )
   }
 }
 
 const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
   
-  const increaseLeft = () => {
-    setAll(allClicks.concat('V'))
-    setLeft(left + 1)
+  const increaseGood = () => {
+    setGood(good + 1)
   }
-  const increaseRight = () => {
-    setAll(allClicks.concat('O'))
-    setRight(right + 1)
+  const increaseNeutral = () => {
+    setNeutral(neutral + 1)
+  }
+  const increaseBad = () => {
+    setBad(bad + 1)
   }
 
   const resetAll = () => {
-    setLeft(0)
-    setRight(0)
-    setAll([])
+    setGood(0)
+    setNeutral(0)
+    setBad(0)
   }
+
+  const allValues = good + neutral + bad
+
+  const averageOfValues = ((good * 1) + (bad * -1)) / (good + neutral + bad)
 
   return (
     <>
-      <Display counter={left}/><Button handleClick={increaseLeft} buttonText="vasen"/>
-      <Button handleClick={increaseRight} buttonText="oikea"/><Display counter={right}/>
-      <History clicks={allClicks}/>
-      <Button handleClick={resetAll} buttonText="resetoi kaikki"/>
+      <h1> Give feedback </h1>
+      <Button handleClick={increaseGood} buttonText="Good"/>
+      <Button handleClick={increaseNeutral} buttonText="Neutral"/>
+      <Button handleClick={increaseBad} buttonText="Bad"/>
+      
+      <h2> Statistics </h2>
+      <Statistics good={good} neutral={neutral} bad={bad} average={averageOfValues} all={allValues} positive={(good / allValues) * 100 + '%'}/>
+      <Button handleClick={resetAll} buttonText="Reset"/>
     </>
   )
 }
