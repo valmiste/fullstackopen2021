f2446448f4ec47e37de20654ece4a9536ffd3cd9
Jyri Ojala <jyri.ojala@web-veistamo.fi>
2022-01-07 20:55:09 +0200

    2.18*: puhelinluettelo step10

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -41,11 +41,13 @@ const Person = (props) => {
   return (
     <li>
       {" "}
-      {props.person.name} {props.person.number}
-      {" "}
-      <button type="button" onClick={() => {
-        props.removePerson(props.person.id, props.person.name)}
-        }>
+      {props.person.name} {props.person.number}{" "}
+      <button
+        type="button"
+        onClick={() => {
+          props.removePerson(props.person.id, props.person.name);
+        }}
+      >
         delete
       </button>
     </li>
@@ -56,7 +58,11 @@ const Persons = (props) => {
   return (
     <>
       {props.contactsToShow.map((person) => (
-        <Person key={person.name} person={person} removePerson={props.removePerson}/>
+        <Person
+          key={person.name + person.id}
+          person={person}
+          removePerson={props.removePerson}
+        />
       ))}
     </>
   );
@@ -79,7 +85,7 @@ const Filter = (props) => {
 
 const App = () => {
   const [persons, setPersons] = useState([
-    // Add enough default data for testing.
+    // Add enough default data for testing. External fetch should overwrite these.
     { name: "Arto Hellas", number: "040-123456" },
   ]);
   const [currentFilter, setNewFilter] = useState("");
@@ -93,49 +99,61 @@ const App = () => {
       setPersons(initialPersonsData);
     };
 
-    // const promiseFromPersons = axios.get("http://localhost:3001/persons")
     const promiseFromPersons = personService.getAll();
     promiseFromPersons.then(handlePersonsData);
   }, []);
 
   const removePerson = (id, personName) => {
-
     if (!window.confirm(`Delete ${personName}`)) return false;
 
-    personService.remove(id)
-    .then( response => {
-      if (response.status === 200) {
-        console.log(`Successfully removed the person with ID ${id}`);
-      }
-    })
-    .catch( error => {
-      console.log(`Having problems with removal, is the ID: ${id} deleted already?`);
-    })
+    personService
+      .remove(id)
+      .then((response) => {
+        if (response.status === 200) {
+          console.log(`Successfully removed the person with ID ${id}`);
+        }
+      })
+      .catch((error) => {
+        console.log(
+          `Having problems with removal, is the ID: ${id} deleted already?`
+        );
+      });
     // Remove missing ID also from Reacts' state.
-    setPersons(persons.filter(person => person.id !== id));
+    setPersons(persons.filter((person) => person.id !== id));
   };
 
   const addNameAndNumber = (event) => {
     event.preventDefault();
-    // Alert user and reset fields in case the name is already in the list.
-    if (persons.findIndex((person) => person.name === newName) > -1) {
-      alert(`${newName} is already added to phonebook`);
-      setNewName("");
-      setNewNumber("");
-      return false;
-    }
+    const existingPerson = persons.find((person) => {
+      // Check if exists, case insensitively & ignore lead/trail whitespace.
+      return person.name.trim().toLowerCase() === newName.trim().toLowerCase()
+    });
 
     const newContactInfoObj = {
       name: newName,
-      // id: persons.length + 1, // JSON-server will create this.
       number: newNumber,
     };
 
+    if (typeof existingPerson === 'object' && existingPerson !== null) {
+      // If user already exists, check if we want to update.
+      if (
+        window.confirm(
+          `${newName} is already added to phonebook, replace the old number with a new one?`)
+      ) {
+        personService.update(existingPerson.id, newContactInfoObj).then( (returnedPersonData) => {
+          setPersons(persons.map(person => person.id !== existingPerson.id ? person : returnedPersonData ));
+        });
+      } 
+      setNewName("");
+      setNewNumber("");
+      return;
+    }
     personService.create(newContactInfoObj).then((updatedPersonsData) => {
       setPersons(persons.concat(updatedPersonsData));
       setNewName("");
       setNewNumber("");
     });
+    return;
   };
 
   // Create filtered list with case insensitive matching.
@@ -161,7 +179,7 @@ const App = () => {
 
       <Filter currentFilter={currentFilter} setNewFilter={setNewFilter} />
 
-      <Persons contactsToShow={contactsToShow} removePerson={removePerson}/>
+      <Persons contactsToShow={contactsToShow} removePerson={removePerson} />
     </div>
   );
 };
